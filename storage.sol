787762627571028d4571deb0e0001dcba0ceb5b1
Manny Urbino <32224520+murbino@users.noreply.github.com>
2017-10-07 20:23:43 -0400

    Added file via upload
    Uploaded storage.sol file.

---
+++ storage.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.0;
+
+/* A simple smart contract that will query the user to enter for an unsigned
+integer and the output will be stored */
+
+// The construct
+contract SimpleStorage {
+    uint storedData;
+
+    //Query for an unsigned integer
+    function set(uint x) {
+        storedData = x;
+    }
+
+    //Store the data
+    function get() constant returns (uint) {
+        return storedData;
+    }
+}
